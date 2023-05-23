e82a56aa9e5da0ab55914db86d8aab15307ffe55
99slayer <21979862+99slayer@users.noreply.github.com>
2023-05-23 16:07:06 -0600

    - Changed pineapple image.

--- src/components/Main.js
+++ src/components/Main.js
@@ -23,7 +23,7 @@ export const Main = (props) => {
     },
     {
       type: 'pineapple',
-      image: 'https://images.pexels.com/photos/4110334/pexels-photo-4110334.jpeg'
+      image: 'https://images.unsplash.com/photo-1558195955-c4751542e89b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80'
     },
     {
       type: 'watermelon',
