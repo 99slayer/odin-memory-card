46e55798ebbd0f48ff1a8253f2e9aaafbc178d1d
99slayer <21979862+99slayer@users.noreply.github.com>
2025-02-28 19:38:12 -0700

    style: Update styles

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,9 +2,9 @@ import React from 'react';
 import '../styles/Card.css';
 
 export const Card = (props) => {
-  const { type, image, clickFunc } = props;
+	const { type, image, clickFunc } = props;
 
-  return (
-    <div id={type} className='card' style={{backgroundImage: `url(${image})`}} onClick={() => { clickFunc(type) }}></div>
-  )
+	return (
+		<div id={type} className='card' style={{ backgroundImage: `url(${image})` }} onClick={() => { clickFunc(type) }}></div>
+	)
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,17 @@ import React from 'react';
 import '../styles/Header.css';
 
 export const Header = (props) => {
-  const { score, highScore } = props;
-  return (
-    <div id='header'>
-      <h1 id='heading'>Memory Game</h1>
-      <p id='rules'>Try to remember the cards you have clicked.</p>
-      <div id='scoreboard'>
-        <div id='current-score'>{`SCORE: ${score}`}</div>
-        <div id='high-score'>{`HIGH SCORE: ${highScore}`}</div>
-      </div>
-    </div>
-  )
+	const { score, highScore } = props;
+	return (
+		<div id='header'>
+			<div>
+				<h1 id='heading'>Memory Game</h1>
+				<p id='rules'>Try and click each card only once. Clicking the same card twice will restart the game.</p>
+			</div>
+			<div id='scoreboard'>
+				<div id='current-score'>{`CURRENT SCORE: ${score}`}</div>
+				<div id='high-score'>{`HIGH SCORE: ${highScore}`}</div>
+			</div>
+		</div>
+	)
 }
